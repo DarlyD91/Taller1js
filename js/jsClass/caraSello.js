7387dedfb5766f6c9374c099b4966137bee79ac8
DarlyDiaz <dydiaz46@misena.edu.co>
2022-09-26 21:49:54 -0500

    Taller 1 JavaScript

---
+++ js/jsClass/caraSello.js
@@ -0,0 +1,34 @@
+let dinero, apuesta=0, cont=0, des, ale;
+
+dinero=prompt("Cantidad general para apostar");
+
+if(opc=true){
+    apuesta=prompt("Ingrese la cantidad que desea apostar");
+    ale=Math.round(Math.random()*2);
+    des=parseInt(prompt("Ingrese su elección 1.Cara 2.Sello"));
+
+    if(ale==1 && des==1){
+        alert("La respuesta es cara, felicidades");
+        dinero=parseInt(dinero)+parseInt(apuesta);
+    }else if(ale==2 && des==2){
+        alert("La respuesta es sello, felicidades");
+        dinero=parseInt(dinero)+parseInt(apuesta);
+    }else if(ale==1 && des==2){
+        alert("La respuesta es cara, Game Over");
+        dinero=parseInt(dinero)-parseInt(apuesta);
+    }else if(ale==2 && des==1){
+        alert("La respuesta es sello,Game Over");
+        dinero=parseInt(dinero)-parseInt(apuesta);
+    }else{
+
+    }
+
+    resp=confirm("Desea continuar?");
+}else if(opc=false){
+alert(`El resultado es ${dinero}`);
+
+    alert(`El resultado es ${marranito}`);
+    alert(`El valor acumulado es ${cont}`);
+    apuesta=parseInt(apuesta)+parseInt(dinero);
+    cont=parseInt(cont)+1;
+}
